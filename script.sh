4b571019f778510ba3345f6c5ac971472e693a71
Luis Thomas <luis.thomas2005@gmail.com>
2020-02-10 21:10:45 +0100

    removed blktrace from script

--- script.sh
+++ script.sh
@@ -7,7 +7,5 @@ mkdir -p /sys/fs/cgroup/memory/OPENCVDNN
 echo $1 >> /sys/fs/cgroup/memory/OPENCVDNN/memory.limit_in_bytes
 cat /sys/fs/cgroup/memory/OPENCVDNN/memory.limit_in_bytes
 
-blktrace/blktrace -d /dev/vdb1 -o dnn &
-
 cd /home/vm/opencvdnn_results/build
-./Detection --config=../yolo_files/yolov3.cfg --model=../yolo_files/yolov3.weights --classes=../yolo_files/coco.names --width=416 --height=416 --scale=0.00392 --input=../yolo_files/fall.mp4 --rgb
+time ./Detection --config=/home/vm/yolo_files/yolov3.cfg --model=/home/vm/yolo_files/yolov3.weights --classes=/home/vm/yolo_files/coco.names --width=416 --height=416 --scale=0.00392 --input=/home/vm/yolo_files/fall.mp4 --rgb
